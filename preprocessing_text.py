24b7832906c352cc5c64df2eed9f66fb98d3eafd
vijayendra <vijayendra@zopper.com>
2017-09-30 21:31:04 +0530

    Update preprocessing_text.py

--- preprocessing_text.py
+++ preprocessing_text.py
@@ -5,3 +5,26 @@ tokens  = [w  for w in word_tokenize(text.lower())
             if w.isalpha()] 
 no_stops = [t for t in tokens
              if t  not in stopwords.words('english')]
+
+
+
+# Import WordNetLemmatizer
+from nltk.stem import WordNetLemmatizer
+
+# Retain alphabetic words: alpha_only
+alpha_only = [t for t in lower_tokens if t.isalpha()]
+
+# Remove all stop words: no_stops
+no_stops = [t for t in alpha_only if t not in english_stops]
+
+# Instantiate the WordNetLemmatizer
+wordnet_lemmatizer = WordNetLemmatizer()
+
+# Lemmatize all tokens into a new list: lemmatized
+lemmatized = [wordnet_lemmatizer.lemmatize(t) for t in no_stops]
+
+# Create the bag-of-words: bow
+bow = Counter(lemmatized)
+
+# Print the 10 most common tokens
+print(bow.most_common(10))
